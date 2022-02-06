754d0d351141cc3da677db00c999be706e9615e6
Mauro de Oliveira Andre <mauroolivere@hotmail.com>
2022-02-06 22:23:51 +0100

    Initial Commit

---
+++ Shared/Aqui_Te_mApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aqui_Te_mApp.swift
+//  Shared
+//
+//  Created by Mauro Sys One on 2/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Aqui_Te_mApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Mauro Sys One on 2/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
